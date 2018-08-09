bec5b4a2cdee79498436598183c485094bd29fdb
Anna Sakowska <annsak2016@gmail.com>
2018-08-09 10:01:13 +0200

    Display markers one more time

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  render () {
+    return (
+      <header >
+        <h2 className="slogan">Neighboor Map</h2>
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import { GoogleApiWrapper, InfoWindow, Marker, Map} from 'google-maps-react'
+import './App.css';
+
 
 let foursquare = require('react-foursquare')({
   clientID: 'TNIDIKHEBFPJR3WMZMUPRLSN4ZO1HM3TTT5AFY4IUVQAM3BT',
@@ -8,216 +10,44 @@ let foursquare = require('react-foursquare')({
 
 
 
-class MapContainer extends Component {
-  state = {
-    locations: [
-      //Add default locations
-      {name: "Kino Nawojka", location: {lat: 52.8446729, lng: 19.1796138}},
-      {name: "Nowe Centrum Lipna", location: {lat: 52.8446632, lng: 19.1770389}},
-      {name: "Park Miejski", location: {lat: 52.8412546, lng: 19.1808583}},
-      {name: "Urząd Miejski", location: {lat: 52.8453857, lng: 19.1834118}},
-      {name: "Liceum Ogólnokształcące", location: {lat:52.8513319, lng:19.1798322}},
-
-    ],
-    query: '',
-    markers: [],
-    infowindow: new this.props.google.maps.InfoWindow(),
-    checkedMarker:null,
-    street: '',
-	  city: '',
-	  phone: ''
-
-  }
-
-
-
-  componentDidMount() {
-    this.loadMap()
-    this.clickList()
-    //Change marker color when it is clicked
-    this.setState({checkedMarker: this.makeMarkerIcon('ffffff')})
-
-    foursquare.venues.getVenues()
-    .then(res => {
-
-      this.setState({street: res.response.venues});
-     this.setState({city: res.response.venues});
-     this.setState({phone: res.response.venues})
-
-  })
-
-  }
-
-  loadMap() {
-    if (this.props && this.props.google) {
-      const {google} = this.props
-      const maps = google.maps
-
-      const mapRef = this.refs.map
-      const node = ReactDOM.findDOMNode(mapRef)
-
-      const mapConfig = Object.assign({}, {
-        center: {lat: 52.845863, lng: 19.181319},
-        zoom: 13,
-        mapTypeId: 'roadmap'
-      })
-
-      this.map = new maps.Map(node, mapConfig)
-      this.addMarkers()
-    } else {
-      window.alert("There is a problem with map loading")
-    }
-  }
-
-  clickList = () => {
-    const clickedItem = this
-    const {infowindow} = this.state
-
-    const displayInfowindow = (event) => {
-      const markerIndex = this.state.markers.findIndex(m => m.title.toLowerCase() === event.target.innerText.toLowerCase())
-      clickedItem.populateInfoWindow(this.state.markers[markerIndex], infowindow)
-    }
-
-    const list = document.querySelector('.list')
-    list.addEventListener('click', function (event) {
-      if(event.target && event.target.nodeName === "LI") {
-        displayInfowindow(event)
-      }
-    })
-  }
-
-  handleChange = (event) => {
-    this.setState({query: event.target.value})
-  }
-
-  addMarkers = () => {
-    const {google} = this.props
-    let {infowindow} = this.state
-    const bounds = new google.maps.LatLngBounds()
-
-    this.state.locations.forEach((location, ind) => {
-      const marker = new google.maps.Marker({
-        position: {lat: location.location.lat, lng: location.location.lng},
-        map: this.map,
-        title: location.name,
-        animation: google.maps.Animation.DROP,
-      })
-
-      marker.addListener('click', () => {
-        this.populateInfoWindow(marker, infowindow)
-      })
-      this.setState((state) => ({
-      markers: [...state.markers, marker]
-      }))
-      bounds.extend(marker.position)
-    })
-    this.map.fitBounds(bounds)
-  }
-
-  populateInfoWindow = (marker, infowindow) => {
-    const defaultMarker = marker.getIcon()
-    const {checkedMarker, markers,
-      street, city, phone
-      } = this.state
-    //const self = this
-
-    if (infowindow.marker !== marker) {
-      if (infowindow.marker) {
-        const index = markers.findIndex(m => m.title === infowindow.marker.title)
-        markers[index].setIcon(defaultMarker)
-                // Foursquare API Client
-                const clientID = 'TNIDIKHEBFPJR3WMZMUPRLSN4ZO1HM3TTT5AFY4IUVQAM3BT';
-                const clientSecret = 'GJXIBH2A2UQJHFJKHWFHRAKSTVMBYNYN44OUQ0VISHFZSJUX';
-                //const $ = require ('jquery')
-                // get JSON request of foursquare data
-                let reqURL = `https://api.foursquare.com/v2/venues/search?ll=${marker.position.lat},${marker.position.lng}&client_id=${clientID}&client_secret=${clientSecret}&v=20180726&query=${this.title}`;
-
-                foursquare.venues.getVenues()
-                .then(res => {
-                  const {infowindow, marker} = this.state
-                  this.setState({street: res.response.venues});
-                  this.setState({city: res.response.venues});
-                  this.setState({phone: res.response.venues})
-
-                }).catch(alert('Something went wrong with foursquare'));
-
-      }
-      marker.setIcon(checkedMarker)
-      infowindow.marker = marker
-
-
-
-      infowindow.setContent(`<h3>${marker.title}</h3>
-        <br> <div class="content">` + street + `</div>` +
-        `<div class="content">` + this.state.city + `</div>` +
-        `<div class="content">` + this.state.phone + `</div>`)
-        console.log(street);
-        console.log(this.state.city);
-      infowindow.open(this.map, marker)
-
-      infowindow.addListener('closeclick', function () {
-        infowindow.marker = marker.setIcon()
-      })
-    }
-  }
-
-  makeMarkerIcon = (markerColor) => {
-    const {google} = this.props
-    let markerImage = new google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +'|40|_|%E2%80%A2',
-    )
-    return markerImage;
-  }
-
+class MapContainer extends React.Component {
   render() {
-    const {markers, query, locations, infowindow} = this.state
+    const {locations, onMarkerClick, infowindow, checkedMarker, location, data, google, markers} = this.props
 
-    if (query) {
-      locations.forEach((location, index) => {
-        if(location.name.toLowerCase().includes(query.toLowerCase())) {
-          markers[index].setVisible(true)
-        } else {
-          if (infowindow.marker === markers[index]) {
-            infowindow.close()
-          }
-          markers[index].setVisible(false)
-        }
-      })
-    } else {
-      locations.forEach((location, index) => {
-        if (markers.length && markers[index]) {
-          markers[index].setVisible(true)
-        }
-      })
-    }
+
 
 
     return(
       <div>
-        <div className="container">
-          <div className="text-input">
+        <Map
+            google={this.props.google}
+            zoom={ 14 }
+            initialCenter = {{
+              lat: 52.845863,
+              lng: 19.181319
+            }}>
 
-            <ul className="list"> {
-              markers.filter(m => m.getVisible()).map((m,i) =>(
-                <li key = {i} className="link">{m.title}</li>
+              {
+                locations.map(( location, i) => (
+                  <Marker
+                    ref={this.props.markersArray}
+                    onClick = {(props, marker) =>
+                      onMarkerClick(props,marker)
+                    }
+                    key={location.name}
+                    id={location.id}
+                    position={
+                      {lat: location.position.lat,
+                      lng: location.position.lng}
+                    } />
                 ))
-            }
-            </ul>
-
-            <input role="search" className="search"
-              type="text"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div role="application" className="map" ref="map">
-            loading map...
-          </div>
-        </div>
+              }
+        </Map>
       </div>
     )
   }
 }
 
-export default MapContainer
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyATLu0j9Yd3EsjqYrQiXCCfTe1HNHPBE_I'
+})(MapContainer)
